fdc31d31a93303461c51fe9620c8cccbb5cf3cb3
Symanovich75632 <ssymano1stu.vistula.edu.pl>
2026-03-30 15:40:58 +0200

    75632 Project: implementacja zmiany style. + new css for buttons

---
+++ script.js
@@ -0,0 +1,10 @@
+const styleLink = document.getElementById('theme-style');
+
+document.getElementById('btn-red').onclick = function() {
+    styleLink.href = 'red.css'; 
+};
+
+
+document.getElementById('btn-green').onclick = function() {
+    styleLink.href = 'green.css';
+};
