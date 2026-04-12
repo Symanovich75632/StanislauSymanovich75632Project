edce4ea2cfcd9b8057c40653ad3994e6a635a9dd
Symanovich75632 <ssymano1stu.vistula.edu.pl>
2026-04-13 00:55:04 +0200

    75632 zad4: dodanie funkcji ukrywania i pokazywania sekcji umiejetnosci

--- script.js
+++ script.js
@@ -5,13 +5,27 @@ const mySection = document.getElementById('umiejetnosci');
 
 
 
-document.getElementById('btn-red').onclick = function() {
+document.getElementById('btn-red').onclick = function() 
+{
     styleLink.href = 'red.css'; 
 };
 
 
-document.getElementById('btn-green').onclick = function() {
+document.getElementById('btn-green').onclick = function()
+{
     styleLink.href = 'green.css';
 };
 
 
+toggleBtn.onclick = function() {
+    if (mySection.style.display === 'none') 
+    {
+        mySection.style.display = 'block';      
+        toggleBtn.textContent = 'Ukryj sekcję'; 
+    }
+     else 
+    {
+        mySection.style.display = 'none';       
+        toggleBtn.textContent = 'Pokaż sekcję'; 
+    }
+}
